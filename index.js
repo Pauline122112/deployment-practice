4733449a54c3e788bb9099245bd5cbe9dcbf2245
Pauline122112 <pauline.design007@gmail.com>
2021-09-02 21:41:47 -0500

    temporarily finished

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+require('dotenv').config()// allows to stash 'artificial env variables' in a file
 const express = require('express')
 const cors = require('cors')
-
+//put info in the .env- never leaves local machine. 
 console.log(process.env.USER)
 console.log(process.env.HOME)
 console.log(process.env.PORT)
 
 //This will work on Heroku and production- Will have a value # in heroku land
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000 //fallback is nice but not required
+//is now deployable to heroku
 
 const server = require('express')()
 
