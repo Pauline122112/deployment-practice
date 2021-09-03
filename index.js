bbd9b601272700707549a5547b6769095e8052f0
Pauline122112 <pauline.design007@gmail.com>
2021-09-02 21:33:48 -0500

    deployable

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+
+console.log(process.env.USER)
+console.log(process.env.HOME)
+console.log(process.env.PORT)
+
+//This will work on Heroku and production- Will have a value # in heroku land
+const PORT = process.env.PORT || 5000
+
 const server = require('express')()
 
-server.listen(5000, () => {
-    console.log('Listening on port 5000')
+server.use(express.json())
+server.use(cors())
+
+server.get('/', (req, res) => {
+    res.send(`<h1>Pauline is the best in the WORLD</h1>`)
+})
+
+server.get('/api', (req, res) => {
+    res.json({ message: 'I believe I can fly'})
+})
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 })
